740cf4fda569f9cd6b43d9b37fb3b4e9285fc86b
chooreto <matveymoto2905@gmail.com>
2024-12-07 14:27:49 +0200

    1

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import sqlite3
+from flask import *
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_tempplate('index.html')
+    return render_template('index.html')
 
 @app.route('/result')
-def result:
+def result():
     connection = sqlite3.connect("database.sqlite")
     cursor = connection.cursor()
     cards = cursor.execute("SELECT * FROM brawlers").fetchall()
     connection.close()
     return render_template('brawlers.html', brawlers=cards)
+
+@app.route('/mythic')
+def mythic():
+    return render_template('mythic.html')
+
+@app.route('/epic')
+def epic():
+    return render_template('epic.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/rare')
+def rare():
+    return render_template('rare.html')
+
+@app.route('/Shelly')
+def Shelly():
+    return render_template('Shelly.html')
+
+@app.route('/Shelly')
+def Shelly():
+    return render_template('Shelly.html')
+
+
+
 
 app.run(debug=True)
 
